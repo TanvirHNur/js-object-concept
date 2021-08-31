8a2169af962a99c94e0d380b4ca44bc6ea0d62f0
TanvirHNur <tanvir.du.ier@gmail.com>
2021-08-31 19:24:58 +0600

    first commit

--- summary.js
+++ summary.js
@@ -12,3 +12,19 @@ const values = Object.values(pen);
 for (const key in pen){
     console.log(pen [key])
 }
+
+
+const a = 1;
+const b = 1;
+const c = a;
+
+
+if(JSON.stringify(a) ===  JSON.stringify(b)){
+    console.log('same')
+}
+
+const aBounded = pen.write.bind(a);
+aBounded();
+// call  appply
+
+// this 
